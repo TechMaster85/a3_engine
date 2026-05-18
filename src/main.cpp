85c4533189eb02e187b2263b22d4fd5c256a451e
William Starrs <35636283+TechMaster85@users.noreply.github.com>
2026-05-18 12:50:33 -0400

    Removed Windows libs/ dll workaround, only four dlls in build result now

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,8 @@
 #include "engine.h"
 
-#ifdef _WIN32
-#include <filesystem>
-#include <iostream>
-#include <windows.h>
-#endif
-
 int main(int argc, char *argv[]) {
     (void)argc;
     (void)argv;
-
-#ifdef _WIN32
-
-    // Windows searches for DLLs in either current directory or system folders
-    // Too many dlls next to an executable is annoying
-    // Windows has no concept of rpath like Unix, so we add the folder manually
-
-    // Get the path of the executable
-    wchar_t buffer[MAX_PATH];
-    GetModuleFileNameW(NULL, buffer, MAX_PATH);
-    std::filesystem::path exePath = std::filesystem::path(buffer).parent_path();
-
-    // Combine with the 'libs' subfolder
-    std::filesystem::path libPath = exePath / "libs";
-
-    // Add to DLL search path
-    if (!SetDllDirectoryW(libPath.c_str())) {
-        std::cerr << "Failed to set DLL directory." << std::endl;
-    }
-#endif
 
     Engine::startup();
     Engine::loop();
